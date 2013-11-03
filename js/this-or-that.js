decb734dca8f2dca50dc28cdc5be44c6b9c985bf
andrex84 <andrex84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-03 08:47:08 +0000

    Hello world
    git-svn-id: https://plugins.svn.wordpress.org/this-or-that/trunk@797990 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/this-or-that.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function(){
+
+	if( jQuery('#this-or-that').length == 0 ){
+		return false
+	}
+
+	set_thisorthat_links();
+	set_keypress();
+
+});
+
+function set_thisorthat_links(){
+
+	jQuery('.this-or-that-btn').click(function(e){
+
+		e.preventDefault();
+
+		if( jQuery(this).parents('.thisthat_column').is(':animated') ){
+			return false; //Make sure you have to wait till the previous animation stopped
+		}
+
+		var link = jQuery(this).attr("href");
+
+		/* ANIMATION */
+
+		//fix the height of the container
+		var h = jQuery(this).parents('#this-or-that').height();
+		jQuery(this).parents('#this-or-that').css({'min-height': h });
+		
+		jQuery(this).parents('.this-or-that_item')
+			.animate({opacity: "0"}, { queue: false, duration: 300 })
+			.toggle("scale",{percent: "140"}, 400, function(){
+				
+				jQuery('#this-or-that').animate({ opacity: '0'}, 200)
+				
+				// Do an AJAX request to save the 'favorite', simultaniously with retrieving the 'new' items.
+				jQuery('#single-project-content').load( link + ' #this-or-that .this-or-that-wrapper', function(){
+				
+				});
+
+					jQuery('#this-or-that').animate({ opacity: '1'}, 200)
+					set_thisorthat_links(); //make sure the links in the loaded HTML
+
+					//jQuery(this).css({height: 'auto' });	
+
+				});
+			
+			});
+	})
+}
+
+function set_keypress(){
+	jQuery("body").keydown(function(e) {
+		if(e.keyCode == 37) { // left
+   			jQuery('.this-or-that_item').eq(0).find("a.this-or-that-btn").first().trigger("click");
+  		} 
+  		if(e.keyCode == 39) { // right
+   			jQuery('.this-or-that_item').eq(1).find("a.this-or-that-btn").first().trigger("click");
+  		} 
+	});
+}
